6c3ab230fde6c684f987662d811b32f85472c56d
Joe Minichino <joe.minichino@gmail.com>
2015-08-04 20:31:34 +0100

    first draft

---
+++ example.js
@@ -0,0 +1,7 @@
+var libbow = require('./index'),
+  bow = libbow.bow,
+  dict = libbow.dict;
+
+var texts = ["I like\n, : ; chocolate", "Chocolate; is great", "I like  --boar ragu"];
+var voc = dict(texts);
+console.log(bow("boar like chocolate", voc), bow("Ragu is great and I like it", voc));
---
+++ index.js
@@ -0,0 +1,48 @@
+(function () {
+
+  function tokenize(text) {
+    return text
+      .replace(/\W/g, ' ')
+      .replace(/\s\s+/g, ' ')
+      .split(' ');
+  }
+
+  function extractDictionary(textArray) {
+    var dict = {},
+      keys = [],
+      words;
+    textArray.forEach(function (text) {
+      words = tokenize(text);
+      words.forEach(function (word) {
+        word = word.toLowerCase();
+        if (!dict[word]) {
+          dict[word] = 1;
+          keys.push(word);
+        } else {
+          dict[word] += 1;
+        }
+      });
+    });
+
+    return {
+      words: keys,
+      dict: dict
+    };
+  }
+
+  function bow(text, vocabulary) {
+    var dict = extractDictionary([text]).dict,
+      vector = [];
+
+    vocabulary.words.forEach(function (word) {
+      vector.push(dict[word] || 0);
+    });
+    return vector;
+  }
+
+  module.exports = {
+    dict: extractDictionary,
+    bow: bow
+  };
+
+}());
